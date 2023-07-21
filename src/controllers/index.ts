55cd901d9fac2b073c257a434d2c4dbfab5d451b
Harshad Kadam <harshad.kadam@sourcefuse.com>
2023-07-21 13:54:31 +0530

    added health controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,4 +1,5 @@
 export * from './ping.controller';
+export * from './health.controller';
 export * from './v1/mutual-funds.controller';
 export * from './v2/mutual-funds.controller';
 export * from './v3/mutual-funds.controller';
